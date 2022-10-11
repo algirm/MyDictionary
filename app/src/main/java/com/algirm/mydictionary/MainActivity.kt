9cfa0c5492dbecceda8b835460a4522f773c11a7
algirm <algirmeong@gmail.com>
2022-10-11 13:46:42 +0700

    init1

--- app/src/main/java/com/algirm/mydictionary/MainActivity.kt
+++ app/src/main/java/com/algirm/mydictionary/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting(name = "Android Compose")
+                    Greeting(name = "Android Compose Test Update Git")
                 }
             }
         }
